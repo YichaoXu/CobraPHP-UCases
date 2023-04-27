18763f6552edd1ebf928ab3d216bce66f5979217
YichaoXu <yxu166@jh.edu>
2023-04-27 01:12:45 -0400

    Update for NOP stmts

--- namespaces/main_func/main_func.php
+++ namespaces/main_func/main_func.php
@@ -1,10 +1,16 @@
 <?php
 
-namespace t {
-    function test(){};
+namespace {
+    function func(){ echo "MAIN SPACE"; }
 }
 
-namespace {
-    t\test();
+namespace TEST1 {
+    func(); // /TEST1/func
+    function func(){ echo "TEST SPACE"; }
+}
+
+
+namespace TEST2 {
+    func(); // /func
 }
 
