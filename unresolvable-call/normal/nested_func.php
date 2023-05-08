de44b3b9a351000ac3ecd403708268bda3cdb420
YichaoXu <yxu166@jh.edu>
2023-05-07 22:52:55 -0400

    Optimisation for the entity creation logic

---
+++ unresolvable-call/normal/nested_func.php
@@ -0,0 +1,12 @@
+<?php
+function test(){
+    function callee_inner(){ echo "inside of func test\n";}
+    function caller(){ callee_outter(); } // line 7
+}
+
+function callee_outter(){ echo "outside of func test\n";}
+
+test();             // line 2
+caller();           // line 4
+callee_inner();     // line 3
+?>
---
+++ unresolvable-call/object/class_new.php
@@ -0,0 +1,13 @@
+<?php
+
+function test(){
+    function callee_inner(){ echo "inside of func test\n";}
+    function caller(){ callee_outter(); } // line 7
+}
+function callee_outter(){ echo "outside of func test\n";}
+
+test(); // line3
+caller(); // line 5
+callee_inner(); // line 4
+?>
+
