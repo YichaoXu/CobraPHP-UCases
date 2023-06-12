f3f75192018db45cb3f906a4cef594e4886d4ea1
YichaoXu <yxu166@jh.edu>
2023-06-11 20:20:58 -0400

    Solve the issue of the global string with `/`, which is not well handled.

--- call_like/function/deepNested.php
+++ call_like/function/deepNested.php
@@ -3,19 +3,20 @@
 namespace A {
     function first_def(){
         function inner_def(){
-            function callee(){ echo "inside of func test\n";}
+            function callee1(){ echo "inside of func test callee1\n";}
+            function callee2(){ echo "inside of func test callee2\n";}
         } // line 7
     }
 }
 
 namespace B {
     \A\first_def();
-
-    function caller(){ echo "outside of func test\n"; \A\callee(); }
+    function caller($callee){ echo "outside of func test\n"; $callee(); }
 }
 
 namespace C{
     \A\inner_def();
-    \B\caller();
+    \B\caller("\A\callee1");
+    \B\caller("\A\callee2");
 }
 
