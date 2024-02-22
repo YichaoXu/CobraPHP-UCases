503ad710259e7efb1956f6cac18a399f7ee115c3
stonehj05 <stonehj@outlook.com>
2024-02-22 01:01:56 -0500

    finish cases for testing FetchValueConsts

---
+++ intra/expr/FetchValueConsts/fetch_classfield_const.php
@@ -0,0 +1,10 @@
+<?php
+
+class Example {
+    const PI = 3.14;
+    const GREETING = "Hello, World!";
+}
+
+echo Example::PI; 
+echo Example::GREETING; 
+?>
---
+++ intra/expr/FetchValueConsts/fetch_global_const.php
@@ -0,0 +1,6 @@
+<?php
+const name = "value"
+
+echo "$name\n"; 
+
+?>
