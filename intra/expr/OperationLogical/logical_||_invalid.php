a32c9222e79e110aaef009ecbe9988a6a00c9142
Ann-G-Tau <xgao35@jh.edu>
2024-02-27 17:12:24 -0500

    feat: added test cases for logical || in OperationLogical

--- intra/expr/OperationLogical/logical_||_invalid.php
+++ intra/expr/OperationLogical/logical_||_invalid.php
@@ -1,1 +1,11 @@
 <?php
+function test_case_0() { echo "'or' works for neither true\n"; }
+function test_case_1() { echo "'or' works for only one true\n"; }
+
+$a = false;
+$b = false;
+
+$bool = (int) ($a || $b);
+$action = 'test_case_'.$bool;
+$action();
+?>
