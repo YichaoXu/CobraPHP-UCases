0240b1c914159cfb156abdd1f02e2d65c6efbd10
Ann-G-Tau <xgao35@jh.edu>
2024-02-27 17:10:57 -0500

    feat: added test cases for logical or in OperationLogical

--- intra/expr/OperationLogical/logical_and_valid.php
+++ intra/expr/OperationLogical/logical_and_valid.php
@@ -1,11 +1,11 @@
 <?php
-function test_case_0() { echo "'&&' works for only one true\n"; }
-function test_case_1() { echo "'&&' works for both true\n"; }
+function test_case_0() { echo "'and' works for only one true\n"; }
+function test_case_1() { echo "'and' works for both true\n"; }
 
 $a = true;
 $b = true;
 
-$bool = (int) ($a && $b);
+$bool = (int) ($a and $b);
 $action = 'test_case_'.$bool;
 $action();
 ?>
