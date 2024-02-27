4b81dd9a4888478c67d7ad95a7b900530bd4b6fd
Ann-G-Tau <xgao35@jh.edu>
2024-02-27 16:55:30 -0500

    feat: modified test cases for logical && in OperationLogical

--- intra/expr/OperationLogical/logical_and.php
+++ intra/expr/OperationLogical/logical_and.php
@@ -1,16 +1,16 @@
 <?php
-function test_case_1() { echo "'and' works for both true\n"; }
-function test_case_0() { echo "'and' works for only one true\n"; }
+function test_case_1() { echo "'&&' works for both true\n"; }
+function test_case_0() { echo "'&&' works for only one true\n"; }
 
 $a = true;
 $b = true;
 $c = false;
 
-$bool1 = (int) ($a and $b);
+$bool1 = (int) ($a && $b);
 $action1 = 'test_case_'.$bool1;
 $action1();
 
-$bool2 = (int) ($a and $c);
+$bool2 = (int) ($a && $c);
 $action2 = 'test_case_'.$bool2;
 $action2();
 ?>
