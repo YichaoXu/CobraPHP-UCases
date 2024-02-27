19d0c527ac0f8926098901704628caa03151dfa7
Ann-G-Tau <xgao35@jh.edu>
2024-02-27 16:54:05 -0500

    feat: modified test cases for logical and in OperationLogical

--- intra/expr/OperationLogical/logical_and.php
+++ intra/expr/OperationLogical/logical_and.php
@@ -1,19 +1,16 @@
 <?php
-function test_case_1() { echo "Both statements are true."; }
-function test_case_2() { echo "1st statement is true, 2nd statement is false."; }
-function test_case_3() { echo "1st statement is false, 2nd statement is true."; }
-function test_case_4() { echo "Both statements are false."; }
+function test_case_1() { echo "'and' works for both true\n"; }
+function test_case_0() { echo "'and' works for only one true\n"; }
 
 $a = true;
 $b = true;
 $c = false;
-$d = false;
-if ($a and $b)
-    test_case_1();
-elseif ($a and $c)
-    test_case_2();
-elseif ($c and $a)
-    test_case_3();
-elseif ($c and $d)
-    test_case_4();
+
+$bool1 = (int) ($a and $b);
+$action1 = 'test_case_'.$bool1;
+$action1();
+
+$bool2 = (int) ($a and $c);
+$action2 = 'test_case_'.$bool2;
+$action2();
 ?>
