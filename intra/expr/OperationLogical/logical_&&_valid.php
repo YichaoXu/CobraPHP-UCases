c3d71864dcc15b74ce418e162c95de5f4c3d56ca
Ann-G-Tau <xgao35@jh.edu>
2024-02-27 16:59:44 -0500

    feat: corrected test cases for logical && in OperationLogical

--- intra/expr/OperationLogical/logical_&&_valid.php
+++ intra/expr/OperationLogical/logical_&&_valid.php
@@ -4,14 +4,9 @@ function test_case_1() { echo "'&&' works for both true\n"; }
 
 $a = true;
 $b = true;
-$c = false;
 
-$bool1 = (int) ($a && $b);
-$action1 = 'test_case_'.$bool1;
-$action1();
-
-$bool2 = (int) ($a && $c);
-$action2 = 'test_case_'.$bool2;
-$action2();
+$bool = (int) ($a && $b);
+$action = 'test_case_'.$bool;
+$action();
 ?>
 
