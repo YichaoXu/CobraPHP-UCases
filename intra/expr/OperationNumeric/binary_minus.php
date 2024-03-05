11635470c636fe236570668171cca6874a1ca255
Ann-G-Tau <xgao35@jh.edu>
2024-03-05 16:26:02 -0500

    feat: added cases for binary plus/minus in OperationNumeric

---
+++ intra/expr/OperationNumeric/binary_minus.php
@@ -0,0 +1,20 @@
+<?php
+function test_case_4() { echo "binary minus worked for ints\n"; }
+function test_case_3() { echo "binary minus worked for floats\n"; }
+
+$a = 6;
+$b = 2;
+$action = 'test_case_'.($a - $b);
+$action();
+
+$action = 'test_case_'.-($b - $a);
+$action();
+
+$a = 7.00;
+$b = 4.00;
+$action = 'test_case_'.($a - $b);
+$action();
+
+$action = 'test_case_'.-($b - $a);
+$action();
+?>
