0d9ad2ddbdd8a217b8a7f3f5ec3a0566a3e9acd2
Ann-G-Tau <xgao35@jh.edu>
2024-03-05 22:22:55 -0500

    feat: modify cases for bitwise left shift in OperationNumeric

--- intra/expr/OperationNumeric/bitwise_left_shift.php
+++ intra/expr/OperationNumeric/bitwise_left_shift.php
@@ -5,7 +5,7 @@
 // The original workshop script seems to be using 32-bits, so I am writing the tests in 32-bits.
 // But I am not sure.
 
-function test_case_0() { echo "bitwise left shift gives 8\n"; }
+function test_case_0() { echo "bitwise left shift gives 0\n"; }
 function test_case_8() { echo "bitwise left shift gives 8\n"; }
 function test_case_1073741824() { echo "bitwise left shift gives 1073741824\n"; }
 function test_case_2147483648() { echo "bitwise left shift gives 2147483648\n"; }
@@ -16,13 +16,13 @@ $action();
 $action = 'test_case_' . (4 << 28); // 1073741824
 $action();
 
-$action = 'test_case_' . -(4 << 29); // -2147483648
+$action = 'test_case_' . -(4 << 29); // -(-2147483648)
 $action();
 
 $action = 'test_case_' . (4 << 30); // 0
 $action();
 
-$action = 'test_case_' . -(-4 << 1); // -(-8) = 8
+$action = 'test_case_' . -(-4 << 1); // -(-8)
 $action();
 
 $action = 'test_case_' . (-4 << 29); // 2147483648
