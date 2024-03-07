ef02cc8eaf1feb1d09e8f3f1d2a82581289ce90d
stonehj05 <stonehj@outlook.com>
2024-03-07 14:54:14 -0500

    test cases for stmt ctrl loop like

---
+++ intra/stmt/CtrlFlowChanging/for.php
@@ -0,0 +1,25 @@
+<?php
+
+class Safe{
+    function suspect(){ echo "SAFE"; }
+}
+
+class Vulnerable {
+    function suspect() { echo $_GET["user_input"]; }
+}
+
+$user_input = new Safe();
+
+$b = 0;
+
+for ($i = 1; $i <= 10; $i++) {
+    $b += i;
+}
+
+if($b == 55) {
+	$user_input = new Vulnerable();
+}
+
+$user_input -> suspect();
+
+?>
