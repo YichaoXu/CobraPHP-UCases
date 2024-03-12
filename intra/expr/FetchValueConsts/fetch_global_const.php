ab0b0f4990c9d781121a2b31050a514c0f2e10a6
YichaoXu <yxu166@jh.edu>
2024-03-11 23:27:38 -0400

    fix: fixed all issues in the php codes caused by the students commits

--- intra/expr/FetchValueConsts/fetch_global_const.php
+++ intra/expr/FetchValueConsts/fetch_global_const.php
@@ -1,18 +1,3 @@
 <?php
-<<<<<<< HEAD
 const name = "value";
-
 echo name . "\n";
-=======
-<<<<<<< HEAD
-const name = "value";
-
-echo name . "\n";
-=======
-const name = "value"
-
-echo "$name\n"; 
->>>>>>> 8895389b311096053731e0ce1c490d8618f6617c
->>>>>>> main
-
-?>
