4397d4190fed3961bc4a0398d11b76dc8aba56d2
YichaoXu <yichao.xu.19@ucl.ac.uk>
2024-03-15 19:43:02 -0400

    fix: update fo the new case with more complex assign and pass mechanism in PHP.

--- intra/expr/FetchDeclareVars/fetch_db_prop.php
+++ intra/expr/FetchDeclareVars/fetch_db_prop.php
@@ -4,14 +4,22 @@ class Core {
     function behavior() { echo "SOME BEH\n"; }
 }
 
-class Inner {
+class InnerA {
     function core() { return new Core(); }
 }
+
+class InnerB {
+    function core() { return new Core(); }
+}
+
 class Outer {
 
     public Core $core;
-    function inner() {
-        $inner = new Inner();
+    function inner($someCond = true) {
+        if($someCond)
+            $inner = new InnerA();
+        else
+            $inner = new InnerB();
         $this -> core = $inner -> core();
         return $this -> core;
     }
