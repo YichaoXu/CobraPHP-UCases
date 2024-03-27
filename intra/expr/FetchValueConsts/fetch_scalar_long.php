5a57b2dba94c20f69bccf017e8ba69bd5e1fdeab
Ann-G-Tau <xgao35@jh.edu>
2024-03-27 16:50:08 -0400

    feat: updated tests on scalar long in FetchValueConsts

--- intra/expr/FetchValueConsts/fetch_scalar_long.php
+++ intra/expr/FetchValueConsts/fetch_scalar_long.php
@@ -5,8 +5,15 @@ function func_26() { echo ("Hexadecimal worked."); }
 function func_255() { echo ("Binary worked."); }
 
 $a = 1234; // decimal number
+$a = "func_".$a;
+$a();
 $a = 0123; // octal number (equivalent to 83 decimal)
+$a = "func_".$a;
+$a();
 $a = 0x1A; // hexadecimal number (equivalent to 26 decimal)
+$a = "func_".$a;
+$a();
 $a = 0b11111111; // binary number (equivalent to 255 decimal)
-
+$a = "func_".$a;
+$a();
 ?>
