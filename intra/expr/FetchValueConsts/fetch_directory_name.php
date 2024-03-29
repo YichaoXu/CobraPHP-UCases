a32787015de0313980366858f681fb9655a8da17
Ann-G-Tau <xgao35@jh.edu>
2024-03-29 15:34:44 -0400

    fix: modified dir & file name tests in FetchValueConsts

--- intra/expr/FetchValueConsts/fetch_directory_name.php
+++ intra/expr/FetchValueConsts/fetch_directory_name.php
@@ -1,6 +1,6 @@
 <?php
-function aFetchValueConsts(){ echo "fetch directory name success\n"; }
+function Consts(){ echo "fetch directory name success\n"; }
 $var = __DIR__;
-$var[0] = "a";
+$var = $var[65].$var[-5].$var[-4].$var[-3].$var[-2].$var[-1];
 $var();
 ?>
--- intra/expr/FetchValueConsts/fetch_file_const.php
+++ intra/expr/FetchValueConsts/fetch_file_const.php
@@ -1,3 +1,6 @@
 <?php
-echo __FILE__;
+function php(){ echo "fetch file name success\n"; }
+$var = __FILE__;
+$var = $var[-3]. $var[-2]. $var[-1];
+$var();
 ?>
