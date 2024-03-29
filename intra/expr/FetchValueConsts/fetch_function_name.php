872d69dd0832d48105e7ce16e5c1dba50501d48d
Ann-G-Tau <xgao35@jh.edu>
2024-03-29 16:02:57 -0400

    fix: modified function name test in FetchValueConsts

---
+++ intra/expr/FetchValueConsts/fetch_function_name.php
@@ -0,0 +1,17 @@
+<?php
+function func_test() { echo "fetch function name success\n"; }
+function func_test_new() { echo "fetch function name success\n"; }
+function test() {
+    $var = "func_".__FUNCTION__;
+    $var();
+}
+test();
+class BaseClass {
+    function test_new() {
+        $var = "func_".__FUNCTION__;
+        $var();
+    }
+}
+$var = new BaseClass();
+$var->test_new();
+?>
