528bfde3809045dbb3fdfb914fdd29c1b6b36c50
Ann-G-Tau <xgao35@jh.edu>
2024-03-29 14:52:46 -0400

    fix: modified class_const and dir worker tests in FetchValueConsts

--- intra/expr/FetchValueConsts/fetch_class_const.php
+++ intra/expr/FetchValueConsts/fetch_class_const.php
@@ -1,8 +1,13 @@
 <?php
-$var = __CLASS__;
-echo $var;
+function func_314(){ echo "fetch PI classfield success\n"; }
+function func_Hello(){ echo "fetch GREETING classfield success\n"; }
 class Example {
-    echo $var = __CLASS__;
-echo $var;
+    const PI = 3.14;
+    const GREETING = "Hello";
 }
+$a = (Example::PI)*100;
+$var = "func_". $a;
+$var();
+$var = "func_". Example::GREETING;
+$var();
 ?>
--- intra/expr/FetchValueConsts/fetch_directory_name.php
+++ intra/expr/FetchValueConsts/fetch_directory_name.php
@@ -2,6 +2,4 @@
 function func_FetchValueConsts(){ echo "fetch line const success\n"; }
 $var = "func_". __DIR__;
 $var();
-$var = "func_". __DIR__;
-$var();
 ?>
