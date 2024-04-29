0123151d37cc702e600be3420f932ff8a479f843
YichaoXu <yxu166@jhu.edu>
2024-04-28 20:13:22 -0400

    feat: update for the ctrlflow

--- intra/expr/CallArrayRelated/arr_param.php
+++ intra/expr/CallArrayRelated/arr_param.php
@@ -1,12 +1,18 @@
 <?php
 
-class SomeClass {
-    function some_method() { echo "some_method"; }
+class SomeClass
+{
+    function some_method($info)
+    {
+        echo "some_method $info\n";
+    }
 }
 
-function some_func($arr){
-    $arr[0]->some_method();
+function some_func($arr, $info)
+{
+    $arr[0]->some_method($info);
 }
 
+$user_input = $_GET["user_input"];
 $some_var = array(new SomeClass());
-some_func($some_var);
+some_func($some_var, $user_input);
