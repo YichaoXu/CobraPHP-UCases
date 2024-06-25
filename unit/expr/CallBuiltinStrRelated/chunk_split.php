8980873bce257d1e2ab91c47f94c4170660f5fb7
nzhong12 <116281404+nzhong12@users.noreply.github.com>
2024-06-24 22:50:03 -0400

    Update chunk_split.php

--- unit/expr/CallBuiltinStrRelated/chunk_split.php
+++ unit/expr/CallBuiltinStrRelated/chunk_split.php
@@ -6,7 +6,7 @@ function test_case_3() { echo "correct result above range\n"; }
 function test_case_4() { echo "wrong result outbound\n"; }
 function test_case_5() { echo "correct result outbound\n"; }
 
-// case 1
+// case 0,1
 $a = "HelloWorld";
 $b = chunk_split($a, 2, "-");
 $c = "He-ll-oW-or-ld-";
@@ -17,7 +17,7 @@ if ($b == $c) {
 $action = 'test_case_'.($d);
 $action();
 
-// case 2
+// case 2,3
 $a1 = "Hello World";
 $b1 = chunk_split($a1, 3, "$3 e!");
 $c1 = "Hel$3 0lo $3 0Wor$3 0ld";
@@ -28,7 +28,7 @@ if ($b1 == $c1) {
 $action = 'test_case_'.($d1);
 $action();
 
-// case 3
+// case 4,5
 $a2 = "Hello World";
 $b2 = chunk_split($a2, 25, "$");
 $c2 = "Hello World$";
