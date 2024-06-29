e78a8ac474cc3ea6daa770479b14ab01f1bcfe8e
nzhong12 <116281404+nzhong12@users.noreply.github.com>
2024-06-28 22:47:02 -0400

    Update bin2hex.php

--- unit/expr/CallBuiltinStrRelated/bin2hex.php
+++ unit/expr/CallBuiltinStrRelated/bin2hex.php
@@ -1,11 +1,11 @@
 <?php
-function test_case_0() { echo "wrong result hw\n"; }
-function test_case_1() { echo "correct result hw\n"; }
-function test_case_2() { echo "wrong result capital\n"; }
-function test_case_3() { echo "correct result capital\n"; }
+function test_case_0() { echo "wrong result hi\n"; }
+function test_case_1() { echo "correct result hi\n"; }
+function test_case_2() { echo "wrong result php\n"; }
+function test_case_3() { echo "correct result php\n"; }
 
 // case 0,1
-$a = "Hello world!";
+$a = "hi";
 $b = bin2hex($a);
 $c = "48656c6c6f20776f726c6421";
 $d = 0;
@@ -16,7 +16,7 @@ $action = 'test_case_'.($d);
 $action();
 
 // case 2,3
-$a1 = "PHP";
+$a1 = "php";
 $b1 = bin2hex($a1);
 $c1 = "504850";
 $d1 = 2;
