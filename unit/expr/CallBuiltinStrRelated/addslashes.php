79f6776edf8d65a6f8fd52ded7d0bfc313703ef1
nzhong12 <116281404+nzhong12@users.noreply.github.com>
2024-07-03 17:43:00 -0400

    Update addslashes.php

--- unit/expr/CallBuiltinStrRelated/addslashes.php
+++ unit/expr/CallBuiltinStrRelated/addslashes.php
@@ -43,6 +43,5 @@ if ($b2 == $c2) {
 	$d2 = 5;
 }
 $action = 'test_case_'.($d2);
-$action();
 $action($sec_data); // not occurred in path generated
 $action($vul_data); // will occur in the path generated
