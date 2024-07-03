fb58446e350b3d40ac49f9ff5fc38ccb87f620b8
Yichao Xu <yxu166@jh.edu>
2024-07-03 16:01:21 -0400

    Update addslashes.php

--- unit/expr/CallBuiltinStrRelated/addslashes.php
+++ unit/expr/CallBuiltinStrRelated/addslashes.php
@@ -1,10 +1,13 @@
 <?php
-function test_case_0() { echo "wrong result O'Reilly\n"; }
-function test_case_1() { echo "correct result O'Reilly\n"; }
-function test_case_2() { echo "wrong result quote backslash\n"; }
-function test_case_3() { echo "correct result quote backslash\n"; }
-function test_case_4() { echo "wrong result doubleslash\n"; }
-function test_case_5() { echo "correct result doubleslash\n"; }
+function test_case_0($param) { echo "wrong result O'Reilly\n". $param; }
+function test_case_1($param) { echo "correct result O'Reilly\n". $param; }
+function test_case_2($param) { echo "wrong result quote backslash\n". $param; }
+function test_case_3($param) { echo "correct result quote backslash\n" . $param; }
+function test_case_4($param) { echo "wrong result doubleslash\n" . $param; }
+function test_case_5($param) { echo "correct result doubleslash\n" . $param; }
+
+$vul_data = $_GET['data'];
+$sec_data = "secret data";
 
 // case 0,1
 $a = "Is your name O'Reilly?";
@@ -14,8 +17,10 @@ $d = 0;
 if ($b == $c) {
 	$d = 1;
 }
-$action = 'test_case_'.($d);
-$action();
+$action = 'test_case_'.($d); // test_case_1
+$action($sec_data); // not occurred in path generated
+$action($vul_data); // will occur in the path generated
+// /:0 -> /:22 -> /:3 -> /:3 
 
 // case 2,3
 $a1 = "\"quote\" and a backslash \\";
@@ -26,7 +31,8 @@ if ($b1 == $c1) {
 	$d1 = 3;
 }
 $action = 'test_case_'.($d1);
-$action();
+$action($sec_data); // not occurred in path generated
+$action($vul_data); // will occur in the path generated
 
 // case 4,5
 $a2 = "\"double\"";
@@ -38,4 +44,3 @@ if ($b2 == $c2) {
 }
 $action = 'test_case_'.($d2);
 $action();
-?>
